9a89cfd44997c26d7807ff663c82579e007c4e00
keveny <92321571+KevenyLima@users.noreply.github.com>
2022-02-20 14:28:00 -0300

    commit add project form

--- meu-projeto/src/components/NewProjects.js
+++ meu-projeto/src/components/NewProjects.js
@@ -1,12 +1,23 @@
-import {useState,useEffect} from 'react'
+import {useHistory} from "react-router-dom"
 import ProjectsForm from "./Projectsform"
 import styles from "./styles/newProjects.module.css"
 function  NewProjects(){
+    const history = useHistory()
+    function createPost(project){
+        project.cost = 0
+        project.servies=[]
+        fetch("http://localhost:5000/projects",{
+            method: 'post',
+            headers:{'Content-type':'application/json'},
+            body: JSON.stringify(project)
+        }).then(resp=>resp.json())
+        .then(data=>console.log(data))
+    }
     return (
         <div className={styles.newProjects_container}>
             <h1>Criar Projeto</h1>
             <p>Criar seu projeto para depois adicionaros serviços </p>
-            <ProjectsForm btnText="Criar projeto"/>
+            <ProjectsForm handleSubmit={createPost} btnText="Criar projeto"/>
         </div>
        
     )
--- meu-projeto/src/components/Projectsform.js
+++ meu-projeto/src/components/Projectsform.js
@@ -2,8 +2,9 @@ import Select from "./select"
 import Input from "./Input"
 import SubmitButton from "./SubmitButton"
 import {useState,useEffect} from "react"
-function ProjectsForm({btnText}){
+function ProjectsForm({btnText,handleSubmit,projectData}){
     const [categories,setCategories] = useState([])
+    const [project,setProject]=useState(projectData||{})
     useEffect(async()=>{
         try {
             const response = await fetch("http://localhost:5000/categories")
@@ -15,11 +16,20 @@ function ProjectsForm({btnText}){
         }
         
     },[])
+    const submit=(e)=>{
+        e.preventDefault()
+        handleSubmit(project)
+    }
+    function handleChange(e){
+        setProject({...project,[e.target.name]:e.target.value})
+        console.log(project)
+    }
+
     return (
-        <form method="GET">
-            <Input type="text" text="nome do projeto" name="name" placeholder="Insira o nome do projeto" />
-            <Input type="number" text="orçamento do projeto " name="budget" placeholder="Insira o valor do orçamento"/>
-            <Select text="Selecione uma categoria" name="id_options" options={categories}/>
+        <form onSubmit={submit} >
+            <Input handleOnChange={handleChange} type="text" text="nome do projeto" name="name" placeholder="Insira o nome do projeto" />
+            <Input handleOnChange={handleChange} type="number" text="orçamento do projeto " name="budget" placeholder="Insira o valor do orçamento"/>
+            <Select handleOnChange={handleChange} text="Selecione uma categoria" name="id_options" options={categories}/>
             <SubmitButton  text={btnText}/>
         </form>
     )
