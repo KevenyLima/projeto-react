2bdecb6de2ce9f54fb5f91405f60f7ba3d964112
keveny <92321571+KevenyLima@users.noreply.github.com>
2022-02-24 18:31:33 -0300

    add backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+app.use(cors())
+
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.json({messagem:"exemplo"})
+})
+
+app.listen(5080,()=>{console.log("esta rodando")})
